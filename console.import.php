0bdf6fa99b4e5787f3975c0416a9c34aaa45a184
Karel Wintersky <karel.wintersky@yandex.ru>
2017-09-24 13:49:54 +0300

    + console.import:  working

--- console.import.php
+++ console.import.php
@@ -63,20 +63,60 @@ if ($svg->svg_parsing_error) {
 
 CLIConsole::echo_status("<strong>Parsing {$arg_source} file...</strong>" . PHP_EOL);
 
-$image_dims = array(
+$image_dims_default = array(
     'width'     =>  0,
     'height'    =>  0,
     'ox'        =>  0,
     'oy'        =>  0
 );
 
+
+
 // Уточняем тип изображения карты - image / vector
-$image_date_actual = $svg->getImageDefinition();
-if (!$image_date_actual) {
-    CLIConsole::echo_status("<font color='yellow'>[INFO]</font>Layout determined as imageless file! ");
+$image_type = CLIConsole::readline('Уточните тип изображения [vector|bitmap] : ', '/^vector|bitmap$/');
+
+// Имя слоя с разметкой регионов
+$svg_layer_paths_name = CLIConsole::readline_default("Уточните имя слоя с разметкой регионов [Paths] : ", '/.*/', "Paths");
+
+CLIConsole::echo_status("<font color='yellow'>[INFO]</font> Имя слоя с разметкой регионов установлено в <font color='cyan'>{}</font>");
+
+// $svg_layer_paths_name = ($svg_layer_paths_name !== "") ? $svg_layer_paths_name : "Paths";
+
+if ($image_type === 'bitmap') {
+    $svg_layer_images_name = CLIConsole::readline_default("Уточните имя слоя с разметкой регионов [Image] : ", '/.*/', "Image");
+    // $svg_layer_images_name = ($svg_layer_images_name !== "") ? $svg_layer_images_name : "Image";
+} else {
+    $svg_layer_images_name = "";
 }
 
-$image_type = CLIConsole::readline('Уточните тип изображения [vector|image] : ', '/^vector|image$/');
+$svg->parse($svg_layer_paths_name, $svg_layer_images_name);
 
+$image_dims_actual = $svg->getImageDefinition() ?? $image_dims_default;
+unset($image_dims_actual['xhref']);
 
+if (!$image_dims_actual) {
+    CLIConsole::echo_status(PHP_EOL . "<font color='yellow'>[INFO]</font> Файл разметки определен как не содержащий информации о связанном изображении."
+    .PHP_EOL
+    ."Скорее всего он прилагается к векторному изображению. Нужно уточнить несколько значений: "
+    .PHP_EOL);
+} else {
+    CLIConsole::echo_status(PHP_EOL . "<font color='yellow'>[INFO]</font> В файле разметки обнаружена информациz о связанном изображении."
+        .PHP_EOL
+        ."Давайте уточним несколько значений: "
+        .PHP_EOL);
+}
+// уточняем параметры изображения
+
+array_walk($image_dims_actual, function($dim_value, $dim_id) use ($image_type){
+    $msg = "Уточните значение <strong>{$dim_id}</strong> [$dim_value] : ";
+
+    if ($dim_value !== 0) {
+        $result = CLIConsole::readline_default($msg, '/.*/', $dim_value);
+    } else {
+        $result = CLIConsole::readline($msg, '/^\d+$/');
+    }
+    return $result;
+});
+
+var_dump($image_dims_actual);
 
