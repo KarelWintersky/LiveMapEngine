41e709e425b67aeabfa753a53e7b62e2c49feefa
Karel Wintersky <karel.wintersky@yandex.ru>
2021-11-18 14:22:05 +0300

    0.9.3
    - CRLF

--- public/index.php
+++ public/index.php
@@ -78,7 +78,9 @@ try {
         SimpleRouter::get('/map:folio/{map_alias}', 'MapsController@view_map_folio');
 
         // получить информацию по региону
-        SimpleRouter::get('/api/getRegionData/{map_alias}/{region_id}', 'RegionsController@view_region_info');
+        // SimpleRouter::get('/api/getRegionData/{map_alias}/{region_id}', 'RegionsController@view_region_info');
+        
+        SimpleRouter::get('/api/get/regiondata', 'RegionsController@view_region_info');
 
         // получить JS-файл описания разметки
         SimpleRouter::get('/js/map/{map_alias}.js', 'MapsController@get_js_map_definition');
